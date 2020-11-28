651d8365f384e043c1811f7eb9d0ba52ad684590
droshev <mladen.droshev@sap.com>
2020-11-28 11:33:08 -0500

    add theming example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ThemeServiceOutput, ThemesService } from '@fundamental-ngx/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'theming-example';
+
+  @Output()
+  themeChanged = new EventEmitter<ThemeServiceOutput>();
+
+  showWeekCount = false;
+  compact = false;
+  markWeekends = false;
+  cssUrl: SafeResourceUrl;
+  cssCustomUrl: SafeResourceUrl;
+  themes = this._themesService.themes;
+  selectedValue1: string;
+
+  constructor(private _themesService: ThemesService){}
+
+  selectTheme(selectedTheme: string): void {
+    this.cssUrl = this._themesService.setTheme(selectedTheme);
+    this.cssCustomUrl = this._themesService.setCustomTheme(selectedTheme);
+
+    this.themeChanged.emit({
+      themeUrl: this.cssCustomUrl,
+      customThemeUrl: this.cssUrl
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FundamentalNgxCoreModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
